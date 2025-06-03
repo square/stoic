eabcfe350536e0fe17436e531388b257be19b957
Tom Mulcahy <tomm@squareup.com>
2025-06-03 14:53:56 -0700

    Rename forEach to forEachInvocation

--- kotlin/android/plugin-sdk/src/main/kotlin/com/squareup/stoic/jvmti/Jvmti.kt
+++ kotlin/android/plugin-sdk/src/main/kotlin/com/squareup/stoic/jvmti/Jvmti.kt
@@ -5,17 +5,17 @@ import java.lang.reflect.Method
 import kotlin.reflect.KFunction
 import kotlin.reflect.jvm.javaMethod
 
-fun KFunction<*>.forEach(block: (frame: StackFrame) -> Unit) {
-  this.javaMethod!!.forEach { frame -> block(frame) }
+fun KFunction<*>.forEachInvocation(block: (frame: StackFrame) -> Unit) {
+  this.javaMethod!!.forEachInvocation { frame -> block(frame) }
 }
 
-fun Method.forEach(block: (frame: StackFrame) -> Unit) {
+fun Method.forEachInvocation(block: (frame: StackFrame) -> Unit) {
   val methodId = VirtualMachine.nativeFromReflectedMethod(this)
   val jvmtiMethod = JvmtiMethod[methodId]
   val location = jvmtiMethod.startLocation
   jvmti.breakpoint(location) { frame -> block(frame) }
 }
 
-fun JvmtiMethod.forEach(block: (frame: StackFrame) -> Unit) {
+fun JvmtiMethod.forEachInvocation(block: (frame: StackFrame) -> Unit) {
   jvmti.breakpoint(startLocation) { frame -> block(frame) }
 }
